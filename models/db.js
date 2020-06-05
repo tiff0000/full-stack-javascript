852341a4715c639b17ba39eab6acb2f96b3c9286
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-06-05 03:12:23 -0400

    fix auth fail issue

--- models/db.js
+++ models/db.js
@@ -1,10 +1,14 @@
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true }, err => {
-  if (!err) {
-    console.log("MongoDB connected");
-  } else {
-    console.log("error: " + err);
+mongoose.connect(
+  process.env.MONGODB_URL,
+  { useNewUrlParser: true, auth: { authdb: "admin" } },
+  err => {
+    if (!err) {
+      console.log("MongoDB connected");
+    } else {
+      console.log("error: " + err);
+    }
   }
-});
+);
 require("./order.model");
