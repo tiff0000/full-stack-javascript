85b4a144b8060490c0271659035820d0e8321e76
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-06-05 01:58:25 -0400

    added mongodb env variable

--- models/db.js
+++ models/db.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://tiff000:tiff000@cluster0-syk1l.mongodb.net/onlineCourseShop?retryWrites=true&w=majority",
-  { useNewUrlParser: true },
-  err => {
-    if (!err) {
-      console.log("MongoDB connected");
-    } else {
-      console.log("error: " + err);
-    }
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true }, err => {
+  if (!err) {
+    console.log("MongoDB connected");
+  } else {
+    console.log("error: " + err);
   }
-);
+});
 require("./order.model");
